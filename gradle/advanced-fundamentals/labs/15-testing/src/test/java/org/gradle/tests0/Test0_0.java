677ebb1ae8c2af81f9573c780f57ad590bd90efa
David Alberto <dalbertom@gmail.com>
2017-08-11 12:54:30 -0400

    lab15 step 4

--- gradle/advanced-fundamentals/labs/15-testing/src/test/java/org/gradle/tests0/Test0_0.java
+++ gradle/advanced-fundamentals/labs/15-testing/src/test/java/org/gradle/tests0/Test0_0.java
@@ -3,6 +3,7 @@ package org.gradle.tests0;
 import org.junit.Test;
 
 public class Test0_0 {
+    @org.junit.Ignore
     @Test
     public void myTest() throws Exception {
         Thread.sleep(150);
