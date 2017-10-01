4446c8c3d2fb3900e2c5d1c10f28f1f98c9d10b0
David Alberto <dalbertom@gmail.com>
2017-10-01 18:33:23 -0400

    python: add more conversion functions

--- python/casting.py
+++ python/casting.py
@@ -1,9 +1,22 @@
 # hello-world.py
 
 '''
-int(variable) - cast to integer
-str(variable) - cast to string
-float(variable) - cast to float
+int(x [,base]) Converts x to an integer. The base specifies the base if x is a string.
+float(x) Converts x to a floating-point number.
+complex(real [,imag]) Creates a complex number.
+str(x) Converts object x to a string representation.
+repr(x) Converts object x to an expression string.
+eval(str) Evalutes a string an returns an object.
+tuple(s) Converts s to a tuple.
+list(s) Converts s to a list.
+set(s) Converts s to a set.
+dict(d) Creates a dictionary. d most be a sequence of (key, value) tuples.
+frozenset(s) Converts s to a frozen set.
+chr(x) Converts an integer to a character.
+unichr(x) Converts an integer to a Unicode character.
+ord(x) Converts a single character to its integer value.
+hex(x) Converts an integer to a hexadecimal string.
+oct(x) Converts an integer to an octal string.
 '''
 
 a = "0"
