dec7c6abc4feef1ab3850c841fb4ee99358a19c4
David Alberto <dalbertom@gmail.com>
2017-10-06 00:06:45 -0400

    python: functions - argument lists and dictionaries

--- python/functions.py
+++ python/functions.py
@@ -10,4 +10,23 @@ def someFunction(a, b):
 
 someFunction(12, 543)
 
+# Default Argument Values
+# https://docs.python.org/3/tutorial/controlflow.html
+def f(a, L=[]):
+  L.append(a)
+  return L
+
+print(f(1))
+print(f(2))
+print(f(3))
+
+# Argument Lists and Dictionaries
+def g(a, *b, **c):
+  print(a)
+  for bb in b:
+    print(bb)
+  for cc in c:
+    print(cc,':',c[cc])
+
+g('hello world', 'hola', 'mundo', en='hello', es='hola')
 # loops.py
