f14625606c530528ffd3b41e439ad461d3403d3c
David Alberto <dalbertom@gmail.com>
2018-10-23 14:46:30 -0400

    kotlin/assignment.kt: Tue Oct 23 14:46:30 EDT 2018

---
+++ kotlin/assignment.kt
@@ -0,0 +1,20 @@
+var variable = "hello"
+variable = "world"
+val value = "final value"
+println variable
+
+val myBoolean = 7 < 4
+
+val b = 4
+val c: Byte = 127
+val s: Short = 32767
+val i: Int = 7
+val l: Long = 2673642864387246
+val l2 = 3L
+val c: Char = 'd'
+
+listOf(1,2,3,4)
+
+var str: String? = null
+println(str?.length)
+// str!!.length
