78fbe77d8d7347429211fddc881fc8fd2454914e
David Alberto <dalbertom@gmail.com>
2018-11-15 21:26:50 -0500

    golang: conditionals

--- golang/src/hello.go
+++ golang/src/hello.go
@@ -10,4 +10,12 @@ func main() {
   y := 7
   sum := x + y
   fmt.Println(sum)
+
+  if x > 6 {
+    fmt.Println("More than 6")
+  } else if x < 2 {
+    fmt.Println("Less than 2")
+  } else {
+    fmt.Println("Neither more than 6 nor less than 2")
+  }
 }
