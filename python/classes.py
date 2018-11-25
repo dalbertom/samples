b4bcb3193e6462efda436e156c6d32e075a77e4b
David Alberto <dalbertom@gmail.com>
2018-11-25 15:44:53 -0500

    python/classes.py: Sun Nov 25 15:44:52 EST 2018

--- python/classes.py
+++ python/classes.py
@@ -21,3 +21,44 @@ print(sam.breed)
 print(sam.species)
 print(Dog.species)
 print(sam.name)
+
+print('\nCircle')
+class Circle(object):
+    pi = 3.14
+
+    def __init__(self, radius=1):
+        self.radius = radius
+
+    def area(self):
+        return Circle.pi * self.radius ** 2 
+
+    def set_radius(self, radius):
+        'This method takes in a radius, and resets the current radius of the circle'
+        self.radius = radius
+
+c = Circle(10)
+print(c.area())
+c.set_radius(100)
+print(c.area())
+
+print('\nBook')
+class Book(object):
+    def __init__(self, title, author, pages):
+        print('A book has been created')
+        self.title = title
+        self.author = author
+        self.pages = pages
+
+    def __del__(self):
+        print('A book has been deleted')
+
+    def __len__(self):
+        return self.pages
+
+    def __str__(self):
+        return 'Title: %s, Author: %s'.format(self.title, self.author)
+
+b = Book('Biography', 'David', 2)
+print(b)
+print(len(b))
+del b
