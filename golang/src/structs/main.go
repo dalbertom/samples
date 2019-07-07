057761e94fb894c91b50d797db279179b4ca59aa
David Alberto <dalbertom@gmail.com>
2019-07-07 13:46:37 -0400

    golang: structs person with contactInfo

--- golang/src/structs/main.go
+++ golang/src/structs/main.go
@@ -2,17 +2,25 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
-	alex := person{"Alex", "Anderson"}
-	alex2 := person{firstName: "Alex", lastName: "Anderson"}
-	var alex3 person
-
-	fmt.Println(alex)
-	fmt.Println(alex2)
-	fmt.Printf("%+v", alex3)
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contact: contactInfo{
+			email:   "jim.party@email.com",
+			zipCode: 12345,
+		},
+	}
+	fmt.Printf("%+v", jim)
 }
