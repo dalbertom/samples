d71e0c0cb101ab4eeb5b86c535361f433838eb82
David Alberto <dalbertom@gmail.com>
2019-07-09 21:00:19 -0400

    golang/src/http/main.go: Tue Jul  9 21:00:19 EDT 2019

---
+++ golang/src/http/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	resp, err := http.Get("http://google.com")
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+	io.Copy(os.Stdout, resp.Body)
+}
