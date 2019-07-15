b2fd3151f5c4f38c24e272fc00e29ea80727922a
David Alberto <dalbertom@gmail.com>
2019-07-15 17:46:10 -0400

    golang: channels part 2

--- golang/src/channels/main.go
+++ golang/src/channels/main.go
@@ -4,24 +4,29 @@ import "fmt"
 import "net/http"
 
 func main() {
-	links := []string {
+	links := []string{
 		"http://google.com",
 		"http://facebook.com",
 		"http://stackoverflow.com",
 		"http://golang.org",
 		"http://amazon.com",
 	}
+	c := make(chan string)
 
 	for _, link := range links {
-		checkLink(link)
+		go checkLink(link, c)
+	}
+
+	for i := 0; i < len(links); i++ {
+		fmt.Println(<-c)
 	}
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
-		fmt.Println(link, "might be down")
+		c <- link + " might be down"
 	} else {
-		fmt.Println(link, "is up")
+		c <- link + " is up"
 	}
 }
