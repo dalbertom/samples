0fe5c5cbc51d7fc30633e84aee6929a225fd57fc
David Alberto <dalbertom@gmail.com>
2019-07-15 17:02:29 -0400

    golang: channels

---
+++ golang/src/channels/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+import "net/http"
+
+func main() {
+	links := []string {
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+		"http://amazon.com",
+	}
+
+	for _, link := range links {
+		checkLink(link)
+	}
+}
+
+func checkLink(link string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "might be down")
+	} else {
+		fmt.Println(link, "is up")
+	}
+}
