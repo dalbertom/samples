982867c02bb8c934e88043ea4306afcc30fb6056
David Alberto <dalbertom@gmail.com>
2019-10-05 10:33:25 -0400

    ruby/learnrubythehardway/ex1.rb: Sat Oct  5 10:33:25 EDT 2019

---
+++ ruby/learnrubythehardway/ex1.rb
@@ -0,0 +1,66 @@
+#!/usr/bin/env ruby
+
+#https://learnrubythehardway.org
+
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing."
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+
+puts "Hens #{25 + 30 / 6}"
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+cars = 100
+space_in_a_car = 4.0
+drivers = 30
+passengers = 90
+cars_not_driven = cars - drivers
+cars_driven = drivers
+carpool_capacity = cars_driven * space_in_a_car
+average_passengers_per_car = passengers / cars_driven
+
+puts "There are #{cars} cars available."
+puts "There are only #{drivers} drivers available."
+puts "There will be #{cars_not_driven} empty cars today."
+puts "We can transport #{carpool_capacity} people today."
+puts "We have #{passengers} to carpool today."
+puts "We need to put about #{average_passengers_per_car} in each car."
+puts "." * 10
+
+print "hello "
+puts "world"
+
+formatter = "%{first} %{second} %{third} %{fourth}"
+puts formatter % {first: 1, second: 2, third: 3, fourth: 4}
+puts formatter % {first: "one", second: "two", third: "three", fourth: "four"}
+puts formatter % {
+  first: "I had this thing.",
+  second: "That you could type up right.",
+  third: "But it didn't sing.",
+  fourth: "So I said goodnight."
+}
+
+puts %q{
+  There's something going on here.
+  With this weird quote
+  We'll be able to type as much as we like.
+  Even 4 lines if we want, or 5, or 6.
+}
+
+puts """
+  There's something going on here.
+  With this weird quote
+  We'll be able to type as much as we like.
+  Even 4 lines if we want, or 5, or 6.
+""" 
+
+print "How old are you? "
+age = gets.chomp.to_i
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weigh? "
+weight = gets.chomp
+puts "So, you're #{age} old, #{height} tall #{weight} heavy."
