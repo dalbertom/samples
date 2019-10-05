33f88ef5287b5f480ea833875bb76591949a9fa8
David Alberto <dalbertom@gmail.com>
2019-10-05 10:33:25 -0400

    ruby/learnrubythehardway/ex13.rb: Sat Oct  5 10:33:25 EDT 2019

---
+++ ruby/learnrubythehardway/ex13.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby -w
+
+# Exercise 13: Parameters, Unpacking, Variables
+
+first, second, third = ARGV
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
