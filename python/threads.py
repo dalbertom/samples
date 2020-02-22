082411b07dd150e142a86b0bb3c8d2ef5416b4f4
David Alberto <dalbertom@gmail.com>
2020-02-22 10:56:39 -0500

    python/threads.py: Sat Feb 22 10:56:39 EST 2020

---
+++ python/threads.py
@@ -0,0 +1,26 @@
+# https://www.youtube.com/watch?v=IEEhzQoKtQU
+# related: thread_pool.py
+# related: process.py
+
+import threading
+import time
+
+start = time.perf_counter()
+
+def do_something(seconds):
+    print(f'Sleeping for {seconds} second(s)')
+    time.sleep(seconds)
+    print('Done')
+
+threads = []
+for _ in range(10):
+    t = threading.Thread(target=do_something, args=[1.5])
+    threads.append(t)
+    t.start()
+
+for t in threads:
+    t.join()
+
+finish = time.perf_counter()
+
+print(f'Finished in {round(finish-start, 2)} second(s)')
