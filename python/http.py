7fa06e3e1f34c58f3a39fc374ff2fc6e73c1f5aa
David Alberto <dalbertom@gmail.com>
2023-02-07 18:23:49 -0800

    python: add requests_cache

--- python/http.py
+++ python/http.py
@@ -1,6 +1,10 @@
-#pip3 install -U requests
+#pip3 install -U requests requests-cache
 
+import datetime
 import requests
+import requests_cache
+
+requests_cache.install_cache(cache_name='cache', backend='sqlite', expire_after=datetime.timedelta(days=1))
 #from requests.auth import HTTPBasicAuth
 #r = requests.get('https://ci00.eng.appianci.net', auth=HTTPBasicAuth('david.alberto', '0d7c57e4ad8f26abfdc2dd8bbef74807'))
 #r = requests.get('https://ci00.eng.appianci.net')
